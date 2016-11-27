5d448858ff7bbd6c443c9b7fde04bba5cc98b70b
Kim S. Ly <lykims@users.noreply.github.com>
2016-11-26 19:23:25 -0500

    Improve routes and fix @user in links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root    'static_pages#home'
+    root    'static_pages#splash'
+    get     '/home',    to: 'static_pages#home'
     get     '/help',    to: 'static_pages#help'
     get     '/about',   to: 'static_pages#about'
     get     '/signup',  to: 'users#new'
